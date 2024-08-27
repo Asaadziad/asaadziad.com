a38f74f4d38d16f857ec547b9791d8663d9f7cee
Asaad ziad <108868994+Asaadziad@users.noreply.github.com>
2024-08-27 15:08:06 +0300

    test

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 func main() {
 	fs := http.FileServer(http.Dir("./public"))
 	http.Handle("/", fs)
-	fmt.Println("Serving static files on http://localhost:8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	fmt.Println("Serving static files on http://localhost:8081")
+	if err := http.ListenAndServe(":8081", nil); err != nil {
 		fmt.Println("Error starting server:", err)
 	}
 
