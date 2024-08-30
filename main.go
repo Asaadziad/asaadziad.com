e01f1a649db7539ad9796d43f8534272c8d288ea
Asaad ziad <108868994+Asaadziad@users.noreply.github.com>
2024-08-30 07:30:44 +0300

    changes

--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+
+	"github.com/Asaadziad/devlog/views"
+	"github.com/Asaadziad/devlog/views/components"
+	"github.com/a-h/templ"
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("./public"))
-	http.Handle("/", fs)
+	resp, err := http.Get("https://api.github.com/users/Asaadziad/repos")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	var body []byte
+	body, err = io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	var r []Repo
+	json.Unmarshal(body, &r)
+	fmt.Println(r[0])
+	projects := components.Projects(r)
+	index := views.Index(projects)
+	// fs := http.FileServer(http.Dir("./public"))
+	http.Handle("/", templ.Handler(index))
 	fmt.Println("Serving static files on http://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Println("Error starting server:", err)
